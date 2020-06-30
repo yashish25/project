746d9f28d744b74bd784344543a345be06bf36cb
ashish yadav <yashish2508@gmail.com>
2020-06-30 19:43:47 +0530

    2nd feature

---
+++ mysite/polls/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+
+
+class RegistrationForm(forms.Form):
+    print("@@@@@@@@")
+    username = forms.CharField(max_length = 100)
+    password = forms.CharField(max_length = 100)
+    email = forms.CharField(max_length = 100)
+    phone = forms.CharField(max_length = 100)
